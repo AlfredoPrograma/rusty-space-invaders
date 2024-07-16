759dfe2ec52ef9638a73f6a094f93b031ffb5687
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-07-16 09:37:56 -0400

    feature: initialize app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use bevy::app::Plugin;
+
+struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        println!("Hello")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    bevy::app::App::new().add_plugins(HelloPlugin).run();
 }
