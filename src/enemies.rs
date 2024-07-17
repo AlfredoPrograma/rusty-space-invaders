b1438382d624c65c9f0fb049cd940949eac34f61
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-07-16 20:04:22 -0400

    feature(enemies): implement spawn asteroids system

--- src/enemies.rs
+++ src/enemies.rs
@@ -1,8 +1,82 @@
-use bevy::prelude::*;
+use bevy::{math::vec3, prelude::*};
+use rand::Rng;
+
+use crate::{
+    default_config::{WINDOW_X_LIMIT, WINDOW_Y_SIZE},
+    prelude::YSpeed,
+};
 pub struct EnemiesPlugin;
 
 impl Plugin for EnemiesPlugin {
     fn build(&self, app: &mut App) {
-        println!("Hello enemies plugin")
+        app.insert_resource(AsteroidSpawnTimer(Timer::from_seconds(
+            ASTEROID_SPAWNER_TRIGGER_INTERVAL,
+            TimerMode::Repeating,
+        )))
+        .add_systems(Update, spawn_asteroids_system);
+    }
+}
+
+#[derive(Resource)]
+struct AsteroidSpawnTimer(Timer);
+
+#[derive(Component)]
+struct Health(pub i32);
+
+#[derive(Component)]
+struct Asteroid;
+
+#[derive(Bundle)]
+struct AsteroidBundle {
+    sprite: SpriteBundle,
+    speed: YSpeed,
+    health: Health,
+    asteroid: Asteroid,
+}
+
+const ASTEROID_SPAWNER_TRIGGER_INTERVAL: f32 = 2.0;
+const ASTEROID_SPEED: f32 = 7.0;
+const ASTEROID_HEALTH: i32 = 30;
+
+impl AsteroidBundle {
+    fn new(start_position: (f32, f32), texture: Handle<Image>) -> Self {
+        let (x_start_position, y_start_position) = start_position;
+
+        Self {
+            asteroid: Asteroid,
+            health: Health(ASTEROID_HEALTH),
+            speed: YSpeed(ASTEROID_SPEED),
+            sprite: SpriteBundle {
+                texture,
+                transform: Transform {
+                    translation: vec3(x_start_position, y_start_position, 0.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        }
+    }
+}
+
+fn spawn_asteroids_system(
+    time: Res<Time>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut asteroids_spawn_timer: ResMut<AsteroidSpawnTimer>,
+) {
+    if asteroids_spawn_timer.0.tick(time.delta()).just_finished() {
+        let should_spawn = rand::random::<bool>();
+
+        println!("Should spawn: {should_spawn}");
+
+        if should_spawn {
+            let asteroid_texture = asset_server.load("big_meteor_gray.png");
+            let start_position = (
+                rand::thread_rng().gen_range(-WINDOW_X_LIMIT..WINDOW_X_LIMIT),
+                rand::thread_rng().gen_range(WINDOW_Y_SIZE / 2.0..(WINDOW_Y_SIZE / 2.0) + 30.0),
+            );
+
+            commands.spawn(AsteroidBundle::new(start_position, asteroid_texture));
+        }
     }
 }
